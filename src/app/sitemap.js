14803dee10f9a94ea0b3b72a9454063c74f48ff9
littlestorylabs <littlestorylabs@gmail.com>
2026-02-19 20:31:08 +0300

    sitemap eklendi

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://littlestorylabs.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://littlestorylabs.com/lalin",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://littlestorylabs.com/books",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://littlestorylabs.com/games",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://littlestorylabs.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
